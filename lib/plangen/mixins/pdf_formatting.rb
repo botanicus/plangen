a80078549150929192324b5890b264c3614baf0a
James C Russell <james@101ideas.cz>
2015-03-24 09:15:41 +0000

    PDF formatting mixin extracted.

---
+++ lib/plangen/mixins/pdf_formatting.rb
@@ -0,0 +1,17 @@
+module PDFFormattingMixin
+  def subtitle(pdf, text)
+    pdf.move_down 10
+    pdf.text(text, style: :italic, size: 11)
+    # pdf.move_down 5
+  end
+
+  def title(pdf, text)
+    pdf.text(text, style: :bold)
+  end
+
+  def line(pdf, options = Hash.new)
+    pdf.move_down 5
+    pdf.text('_' * 52, options)
+    pdf.move_down 5
+  end
+end
--- lib/plangen/schedule.rb
+++ lib/plangen/schedule.rb
@@ -1,26 +1,9 @@
 require 'plangen/tasklist'
+require 'plangen/mixins/pdf_formatting'
 
 class NoScheduleFoundError < StandardError
   def initialize(day)
     super("No schedule found for #{day}.")
-  end
-end
-
-module PDFFormattingMixin
-  def subtitle(pdf, text)
-    pdf.move_down 10
-    pdf.text(text, style: :italic, size: 11)
-    # pdf.move_down 5
-  end
-
-  def title(pdf, text)
-    pdf.text(text, style: :bold)
-  end
-
-  def line(pdf, options = Hash.new)
-    pdf.move_down 5
-    pdf.text('_' * 52, options)
-    pdf.move_down 5
   end
 end
 
