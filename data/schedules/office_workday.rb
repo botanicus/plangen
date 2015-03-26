1488ceec56e7cfd09b182085e09ad7578d288b2a
James C Russell <james@101ideas.cz>
2015-03-26 17:53:38 +0000

    Class hierarchy reworked.

--- data/schedules/office_workday.rb
+++ data/schedules/office_workday.rb
@@ -2,30 +2,72 @@ class OfficeWorkdaySchedule < Schedule
   HEADER_COLOUR = 'ff0000'
 
   def self.match?(month, day)
-    day.workday?
-  end
-
-  def schedule_items #####
-    puts 'REDEF schedule'
-    super
+    false
   end
 
   def setup
-    # TODO: Make the schedule accessible.
     self.tasks.list('20 MM', :twenty_miles_march, 1)
-    # self.tasks.list('Urgencies & Appointments (<i>hopefully empty most of the days</i>)', :urgencies_and_appointments)
     self.tasks.list('Lunch Break', :lunchbreak, 3)
     self.tasks.list('Evening', :evening, 3)
+  end
+end
 
-    if day.tuesday? || day.thursday?
-      self.tasks.lunchbreak.push('Go swimming.')
-    end
+class OfficeMondaySchedule < OfficeWorkdaySchedule
+  def self.match?(month, day)
+    day.workday? && day.monday?
+  end
+end
 
-    if self.day.thursday?
-      # TODO: Where to put this? schedule? Elsewhere?
-      # TODO: Make this_week etc area accessible.
-      self.tasks.lunchbreak.unshift('Fasting.')
-      self.tasks.evening.push('Relax at the evening. Go to bed early.')
-    end
+class OfficeTuesdaySchedule < OfficeWorkdaySchedule
+  def self.match?(month, day)
+    day.workday? && day.tuesday?
+  end
+
+  def setup
+    super
+
+    self.tasks.lunchbreak.push('Go swimming.')
+  end
+end
+
+class OfficeWednesdaySchedule < OfficeWorkdaySchedule
+  def self.match?(month, day)
+    day.workday? && day.wednesday?
+  end
+
+  def setup
+    super
+
+    self.tasks.lunchbreak.push('Write a blog post.')
+  end
+end
+
+class OfficeThursdaySchedule < OfficeWorkdaySchedule
+  def self.match?(month, day)
+    day.workday? && day.thursday?
+  end
+
+  def setup
+    super
+
+    self.tasks.lunchbreak.push('Go swimming.')
+
+    # TODO: Where to put this? schedule? Elsewhere?
+    # TODO: Make this_week etc area accessible.
+    self.tasks.lunchbreak.unshift('Fasting.')
+    self.tasks.evening.push('Relax at the evening. Go to bed early.')
+  end
+end
+
+class OfficeFridaySchedule < OfficeWorkdaySchedule
+  def self.match?(month, day)
+    day.workday? && day.friday?
+  end
+
+  def setup
+    super
+
+    self.schedule_items.delete('21:00')
+    self.schedule_items.delete('21:40')
   end
 end
